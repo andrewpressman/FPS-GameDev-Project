25539a68d81caf1c706d5ade4aa674f25463405f
andrewpressman <ap@b-p.com>
2023-07-18 10:35:44 -0400

    Switched to c++ project / reorganized file structure

---
+++ Source/fpsGame/fpsGameGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "fpsGameGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AfpsGameGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AfpsGameGameMode();
+};
+
+
+
